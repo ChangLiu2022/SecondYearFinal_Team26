d5582d735dd94dbb22df86bde47376bda42a180e
ChangLiu <98478845+ChangLiu2022@users.noreply.github.com>
2023-05-29 13:59:37 +0100

    Update Angle2Distance.py

--- Angle2Distance.py
+++ Angle2Distance.py
@@ -1,7 +1,9 @@
 import math
 
 def angle2distance(西塔, phi, 阿尔法, 甲, 乙):
-    上边 = 甲*乙*math.sin(phi+阿尔法)-(甲^2)*math.sin(phi)
-    下边 = math.sqrt(甲^2*(math.sin(phi))^2+乙^2*(math.sin(西塔))^2+2*甲*乙*math.sin(西塔)*math.sin(phi)*math.cos(阿尔法+phi-西塔))
+    上边 = 甲*乙*math.sin(phi+阿尔法)-pow(甲)*math.sin(phi)
+    下边 = math.sqrt(pow(甲)*pow((math.sin(phi)))+pow(乙)*pow((math.sin(西塔)))+2*甲*乙*math.sin(西塔)*math.sin(phi)*math.cos(阿尔法+phi-西塔))
     丙 = 上边/下边
     return 丙
+
+print(angle2distance(0.34,0.41,0.8,600,700))
